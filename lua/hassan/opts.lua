0630001515b1f9798e89abb7490cb29ad6fa685f
= <hassanhafiz44@gmail.com>
2023-01-24 18:30:41 +0500

    Refactored

--- lua/hassan/opts.lua
+++ lua/hassan/opts.lua
@@ -1,32 +1,30 @@
-vim.o.ai=true
-vim.o.ar=true
-vim.o.ch=2
-vim.o.cc=80
-vim.o.cuc=true
-vim.o.cul=true
-vim.o.enc='utf-8'
-vim.o.et=true
-vim.o.fenc='utf-8'
-vim.o.gcr=''
-vim.o.hls=false
-vim.o.ic=true
-vim.o.is=true
-vim.o.mis=10
-vim.o.mouse="a"
-vim.o.nu=true
-vim.o.rnu=true
-vim.o.so=10
-vim.o.scl="yes"
-vim.o.scs=true
-vim.o.si=true
-vim.o.sta=true
-vim.o.sol=true
-vim.o.swapfile=false
-vim.o.tabstop=2
-vim.o.sw=2
-vim.o.ut=300
-vim.o.wrap=false
-vim.o.tgc=true
-vim.opt.path:append { '**' }
-vim.opt.wildignore:append { '*/node_modules/*', '*/vendor/*' }
-vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
+local o = vim.opt
+
+o.autoindent = true
+o.autoread = true
+o.cmdheight = 2
+o.cursorcolumn = true
+o.cursorline = true
+o.expandtab = true
+o.fileencoding = 'utf-8'
+o.guicursor = ''
+o.hlsearch = false
+o.ignorecase = true
+o.incsearch = true
+o.mouse = 'a'
+o.number = true
+o.relativenumber = true
+o.scrolloff = 10
+o.signcolumn = "yes"
+o.smartcase = true
+o.smartindent = true
+o.smarttab = true
+o.swapfile = false
+o.tabstop = 2
+o.shiftwidth = 2
+o.updatetime = 300
+o.wrap = false
+o.termguicolors = true
+o.path:append { '**' }
+o.wildignore:append { '*/node_modules/*', '*/vendor/*' }
+o.clipboard:append { 'unnamed', 'unnamedplus' }
--- lua/hassan/plugins.lua
+++ lua/hassan/plugins.lua
@@ -1,25 +1,42 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use 'L3MON4D3/LuaSnip'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/nvim-cmp'
-  use 'neovim/nvim-lspconfig'
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  } use 'williamboman/mason.nvim'
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-      ts_update()
-    end,
-  }
-  use 'folke/tokyonight.nvim'
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use 'L3MON4D3/LuaSnip'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/nvim-cmp'
+	use 'neovim/nvim-lspconfig'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	} use 'williamboman/mason.nvim'
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
+	}
+	use 'folke/tokyonight.nvim'
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
